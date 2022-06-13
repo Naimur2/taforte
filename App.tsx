b4fb042f9dd1a2de963aaae9ae7c5e68fcdc178e
Naimur Rahaman <naimurrahaman248@gmail.com>
2022-06-13 14:31:38 +0600

    updates

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+    const [loaded, error] = useFonts({
+        Montserrat: require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>

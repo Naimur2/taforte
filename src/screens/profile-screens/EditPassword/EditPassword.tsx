bb6570948cda61383228ea03e72046fcc082270f
Naimur Rahaman <naimurrahaman248@gmail.com>
2022-07-16 23:43:11 +0600

    updates

--- src/screens/profile-screens/EditPassword/EditPassword.tsx
+++ src/screens/profile-screens/EditPassword/EditPassword.tsx
@@ -12,7 +12,11 @@ import { Spacing } from "../../../styled/spacing";
 const EditPassword = () => {
     const schema = Yup.object().shape(
         {
-            name: Yup.string().required("Name is required"),
+            oldPassword: Yup.string().required("Old Password is required"),
+            newPassword: Yup.string().required("New Password is required"),
+            confirmPassword: Yup.string()
+                .oneOf([Yup.ref("newPassword"), null], "Passwords must match")
+                .required("Confirm Password is required"),
         },
         {
             abortEarly: false,
@@ -20,7 +24,9 @@ const EditPassword = () => {
     );
 
     const initialValues = {
-        name: "",
+        oldPassword: "",
+        newPassword: "",
+        confirmPassword: "",
     };
 
     const formik = useFormik({
@@ -48,12 +54,32 @@ const EditPassword = () => {
                 <VStack pb={30}>
                     <Input
                         mt={"10%"}
-                        label="Enter name"
-                        placeholder="Enter your name"
-                        value={values.name}
-                        onChangeText={handleChange("name")}
-                        error={touched.name && errors.name}
-                        onBlur={() => setFieldTouched("name")}
+                        label="Enter old Password"
+                        placeholder="Enter your old Password"
+                        value={values.oldPassword}
+                        onChangeText={handleChange("oldPassword")}
+                        error={touched.oldPassword && errors.oldPassword}
+                        onBlur={() => setFieldTouched("oldPassword")}
+                    />
+                    <Input
+                        mt={"10%"}
+                        label="Enter new Password"
+                        placeholder="Enter your new Password"
+                        value={values.newPassword}
+                        onChangeText={handleChange("newPassword")}
+                        error={touched.newPassword && errors.newPassword}
+                        onBlur={() => setFieldTouched("newPassword")}
+                    />
+                    <Input
+                        mt={"10%"}
+                        label="Confirm Password"
+                        placeholder="Confirm your new Password"
+                        value={values.confirmPassword}
+                        onChangeText={handleChange("confirmPassword")}
+                        error={
+                            touched.confirmPassword && errors.confirmPassword
+                        }
+                        onBlur={() => setFieldTouched("confirmPassword")}
                     />
 
                     <Button onPress={handleSubmit} mt={"30%"} text="Save" />
--- src/screens/profile-screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/profile-screens/ProfileScreen/ProfileScreen.tsx
@@ -5,6 +5,8 @@ import Screen from "../../../components/Screen";
 import { Spacing } from "../../../styled/spacing";
 import { Text } from "../../../styled/typography";
 import InputCard from "../InputCard/InputCard";
+import { useNavigation } from "@react-navigation/native";
+import colors from "../../../themes/colors";
 
 // data name,email,contact,address,password
 
@@ -13,55 +15,88 @@ const userData = {
     email: "woman@femail.com",
     contact: "+1325922132222",
     address: "",
+    dob: "22 April, 1997",
 };
 
 const fields = [
     {
         label: "Name",
         value: userData?.name,
-        navigate: "editName",
+        navigate: "EditName",
     },
     {
         label: "Email",
         value: userData?.email,
-        navigate: "editEmail",
+        navigate: "EditEmail",
     },
     {
         label: "Contact",
         value: userData?.contact,
-        navigate: "editContact",
+        navigate: "EditContact",
     },
     {
         label: "Address",
         value: userData?.address,
-        navigate: "editAddress",
+        navigate: "EditAddress",
+    },
+    {
+        label: "Date of Birth",
+        value: userData?.dob,
+        navigate: "EditDOB",
     },
     {
         label: "Password",
         value: "**************",
-        navigate: "editPassword",
+        navigate: "EditPassword",
     },
 ];
 
 const ProfileScreen = () => {
+    const navigation = useNavigation();
+
     return (
         <Screen pt={30} title={"Profile"} leftIcon={false}>
-            <Text px={16} pb={16} fontSize={18} fontWeight={700}>
-                Profile Information
-            </Text>
             <ScrollView
                 contentContainerStyle={{
                     paddingBottom: 100,
                 }}
                 showsVerticalScrollIndicator={false}
             >
+                <Profile mx={20} mb={20}>
+                    <Avatar
+                        source={{
+                            uri: "https://graphonomics.net/wp-content/uploads/2019/07/Smiling-Man-4.jpg",
+                        }}
+                    />
+                    <Text fontSize={16} fontWeight={600} mt={16} mx="auto">
+                        {userData.name}
+                    </Text>
+                    <Text
+                        color={colors.gray200}
+                        fontSize={14}
+                        fontWeight={400}
+                        mt={6}
+                        mx="auto"
+                    >
+                        {userData.dob}
+                    </Text>
+                </Profile>
+
+                <Text px={16} pb={16} fontSize={18} fontWeight={700}>
+                    Profile Information
+                </Text>
+
                 <Container px={16}>
                     {fields.map((field, index) => (
                         <InputCard
                             key={index}
                             value={field.value || "Not Added"}
                             label={field.label}
-                            onEdit={() => console.log("edit")}
+                            onEdit={() =>
+                                navigation.navigate(field.navigate, {
+                                    value: field.value,
+                                })
+                            }
                         />
                     ))}
                 </Container>
@@ -74,4 +109,20 @@ export default ProfileScreen;
 
 const Container = styled.View`
     ${Spacing}
+`;
+
+const Profile = styled.View`
+    border: 1px solid ${colors.light200};
+    padding: 20px;
+    border-radius: 20px;
+    position: relative;
+
+    ${Spacing}
+`;
+
+const Avatar = styled.Image`
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    margin: 0 auto;
 `;
